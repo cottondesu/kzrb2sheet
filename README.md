af9e8f5164d34e178c6c1949f2ab2f463311b471
cottondesu <horykai@gmail.com>
2019-05-08 22:48:54 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+- Google Apps Script which copies [kanazawa.rb events](https://meetup.kzrb.org/) to spreadsheet.
+- Using [clasp](https://github.com/google/clasp)
+- Using TypeScript
+
+## Requirements
+
+- `npm install -g @google/clasp`
+
+## Usage
+
+- `clasp login`
+- prepare `.clasp.json`
+  - `{"scriptId":"XXXXXXXXXXXXXXXXXXXXXX"}`
+- `clasp push`
---
+++ main.ts
@@ -0,0 +1,56 @@
+function onOpen() {
+    const sheet = SpreadsheetApp.getActiveSpreadsheet();
+    const entries = [
+        {
+            functionName: "syncEvents",
+            name: "Kanazawa.rb から取得",
+        },
+    ];
+    sheet.addMenu("スクリプト実行", entries);
+    // メインメニュー部分に[スクリプト実行]メニューを作成して、
+    // 下位項目のメニューを設定している
+}
+
+function syncEvents() {
+    const topUrl = "https://meetup.kzrb.org";
+    const respMainObj = UrlFetchApp.fetch(topUrl);
+    const html = respMainObj.getContentText("UTF-8");
+    const events = html.match(/<li><a href=".\/([\d]*)\/">([\s\S]*?)<\/a><\/li>/g);
+
+    const cells = events.map((event: any) => ([
+        getEventTitle(event),
+        getEventContent(event, topUrl),
+        getEventDate(event),
+        getEventURL(event, topUrl),
+    ]));
+
+    const book = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = book.getSheetByName("一覧");
+
+    sheet.getRange(2, 1, cells.length, 4).setValues(cells);
+}
+
+function getEventTitle(event: any) {
+    return `meetup ${event.match(/[#][0-9]*/g)}`;
+}
+
+function getEventContent(event: any, url: string) {
+    // meetup41以前のイベント内容を除外
+    const EVENT_CONTENTS_EXCLUSION_AFTER_MEETUP = 41;
+    let eventName = "";
+    if (event.match(/\d{1,2}/) > EVENT_CONTENTS_EXCLUSION_AFTER_MEETUP) {
+        let respSubObj = UrlFetchApp.fetch(`${url}${event.match(/\/([\d]*)\//g)}`);
+        let contentHtml = respSubObj.getContentText("UTF-8");
+        let eventTitle = contentHtml.match(/<p>([\s\S]*?)<\/p>/g);
+        eventName = eventTitle[0].replace(/(<p>|<\/p>)/g, "");
+    }
+    return eventName;
+}
+
+function getEventDate(event: any) {
+    return event.match(/\d{4}-\d{1,2}-\d{1,2}/g);
+}
+
+function getEventURL(event: any, url: string) {
+    return `${url}${event.match(/\/([\d]*)\//g)}`;
+}
